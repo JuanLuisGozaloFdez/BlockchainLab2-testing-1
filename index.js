b63beaac33fe6443bafed36036ee74757826c74a
jgozalo <juanluis.gozalo@huanlurobotics.com>
2022-02-08 09:28:02 +0100

    fix files

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 // setup boilerplate
-const app = express();
 let endpoint = ''
+const app = express();
 
-app.get('/', (req, res) {
-    res.send("Ejemplo CI/CD sencillo usando NodeJs+Express desde la Clase Onlinel");
-});
+app.get('/', function(req, res) {
+    // res.send("Ejemplo CI/CD sencillo usando NodeJs+Express desde la Clase Online");
+    res.sendFile(path.join(__dirname + '/index.html')); // serve static html
+})
 
-const PORT = process.evn.PORT || 5000;
+const PORT = 8000;
 app.listen(PORT,function() {
     console.log(`App listening on port ${PORT}`)
 });
